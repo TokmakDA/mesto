e04af8a17cfaa651dff6d9a050354a465c90141e
Dmitry Tokmak <dimka.listok@gmail.com>
2022-11-18 12:48:42 +0400

    fexed 1 iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupSaveButtonElement = popupElement.querySelector('.popup__submit');
 const profileInfo = document.querySelector('.profile');
-const profileEditButtonElement = profileInfo.querySelector('.profile__edit-button');
+const profileEditButtonElement = profileInfo.querySelector(
+  '.profile__edit-button'
+);
 let profileName = profileInfo.querySelector('.profile__name');
 let profileJob = profileInfo.querySelector('.profile__job');
 let formElement = popupElement.querySelector('.popup__form');
 let nameInput = formElement.querySelector('#name');
 let jobInput = formElement.querySelector('#job');
 
-const addPopup = function() {
+function addPopup() {
   popupElement.classList.add('popup_is-opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
-const removePopup = function() {
+function removePopup() {
   popupElement.classList.remove('popup_is-opened');
 }
 
-profileEditButtonElement.addEventListener('click' , addPopup);
-popupCloseButtonElement.addEventListener('click' , removePopup);
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    removePopup();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  removePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+profileEditButtonElement.addEventListener('click', addPopup);
+popupCloseButtonElement.addEventListener('click', removePopup);
