54c5c29626e9b6377ed5c9bb210b975c5772b451
Dmitry Tokmak <dimka.listok@gmail.com>
2022-11-27 17:08:47 +0400

    Six cards from JS array

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,73 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 profileEditButtonElement.addEventListener('click', addPopup);
 popupCloseButtonElement.addEventListener('click', removePopup);
+
+
+// 5 спринт
+
+const initialCards = [
+  {
+    name: 'Калининград',
+    link: './images/Kalinigrad.jpeg'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: './images/St_Petersburg.jpeg'
+  },
+  {
+    name: 'Нижний Новгород',
+    link: './images/NizhnyNovgorod.jpeg'
+  },
+  {
+    name: 'Дюкинский карьер',
+    link: './images/DukinQuarry.jpeg'
+  },
+  {
+    name: 'Владимир',
+    link: './images/Vladimir-Russian-village.jpeg'
+  },
+  {
+    name: 'Выборг',
+    link: './images/Vyborg.jpeg'
+  }
+];
+
+const cardsListElement = document.querySelector('.cards')
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card')
+
+function createCards (item) {
+
+  const card = cardTemplate.cloneNode(true);
+  const cardName = card.querySelector('.card__mane-card');
+  const cardDeleteButton = card.querySelector('.card__trash');
+  const cardLikeButton = card.querySelector('.card__like-button');
+  const imageCard = card.querySelector('.card__image');
+  imageCard.src = item.link;
+  imageCard.alt = cardName.textContent =  item.name;
+  // cardName.textContent = item.mane;
+
+
+  // Обработчики кликов для кнопок лайка и удаления
+  cardDeleteButton.addEventListener('click', handleDeleteButtonClick)
+  cardLikeButton.addEventListener('click', handleCardLikeButtonClick)
+
+  return card;
+}
+
+const handleCardLikeButtonClick = (e) => {
+  e.target.classList.toggle('card__like-button_active')
+}
+
+
+const handleDeleteButtonClick = (e) => {
+  e.target.closest('.card').remove()
+}
+
+const renderCard = (item, wrapElement) => {
+  const element = createCards(item);
+  wrapElement.append(element);
+}
+
+initialCards.forEach(function(item) {
+  renderCard(item, cardsListElement);
+});
